7ecd06bc93fa02eea993cc69ec81e6200601d409
Rahul Chavan <32196872+rahulc27@users.noreply.github.com>
2026-06-28 16:26:40 +0530

    Add: combination-target-sum-ii - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    private List<List<Integer>> res = new ArrayList();
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        combinationSum(0, candidates, new ArrayList(), 0, target);
+        return res;
+    }
+
+    private void combinationSum(int index, 
+                                int[] candidates, 
+                                List<Integer> currComb, 
+                                int currSum, 
+                                int target) {
+        
+        if (currSum == target) {
+            res.add(new ArrayList(currComb));
+            return;
+        }
+        if (currSum > target || index >= candidates.length)
+            return;
+        
+        currComb.add(candidates[index]);
+        combinationSum(index+1, candidates, currComb, currSum + candidates[index], target);
+        currComb.removeLast();
+        int currElement = candidates[index];
+        while(index < candidates.length && candidates[index] == currElement)
+            index++;
+
+        combinationSum(index, candidates, currComb, currSum, target);
+
+    }
+}
