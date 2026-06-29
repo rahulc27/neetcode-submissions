cea5597021b21d790fc85c75ce2ad1730965e466
Rahul Chavan <32196872+rahulc27@users.noreply.github.com>
2026-06-29 20:10:01 +0530

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.java
@@ -0,0 +1,32 @@
+class Solution {
+    List<Integer> dx = new ArrayList(List.of(1,-1,0,0));
+    List<Integer> dy = new ArrayList(List.of(0,0,-1,1));;
+
+    public boolean exist(char[][] board, String word) {
+        boolean isExist = false;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                isExist = isExist || isExist(0, i, j, board, word);
+            }
+        }
+
+        return isExist;
+    }
+
+    private boolean isExist(int index, int x, int y, char[][] board, String word) {
+        if (index >= word.length())
+            return true;
+        
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length)
+            return false;
+        boolean isExist = false;
+        if (board[x][y] == word.charAt(index)) {
+            board[x][y] = '#';
+            for (int i = 0; i < 4; i++) {
+                isExist = isExist || isExist(index+1, x + dx.get(i), y + dy.get(i), board, word);
+            }
+            board[x][y] = word.charAt(index);
+        }
+        return isExist;
+    }
+}
