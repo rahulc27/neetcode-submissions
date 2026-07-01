38768e335edf9527f180866a90b6c4eef82ff74c
Rahul Chavan <32196872+rahulc27@users.noreply.github.com>
2026-07-01 22:31:05 +0530

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.java
@@ -0,0 +1,49 @@
+class Solution {
+    int[] dx = {1,-1,0,0};
+    int[] dy = {0,0,1,-1};
+    public int orangesRotting(int[][] grid) {
+
+        Queue<int[]> nodeQueue = new LinkedList();
+
+        for(int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                if (grid[row][col] == 2) {
+                    grid[row][col] = -2;
+                    nodeQueue.add(new int[]{row,col});
+                }        
+                if (grid[row][col] == 1)
+                    grid[row][col] = -1;
+            }
+        }
+
+        int level = 0;
+
+        while(!nodeQueue.isEmpty()) {
+            int currLevelSize = nodeQueue.size();
+
+            for(int i = 0; i < currLevelSize; i++) {
+                int[] currNode = nodeQueue.poll();
+                for(int j = 0; j < 4; j++) {
+                    int x = currNode[0] + dx[j];
+                    int y = currNode[1] + dy[j];
+                    if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == -1) {
+                        grid[x][y] = level + 1;
+                        nodeQueue.add(new int[]{x,y});
+                    }
+                }
+                
+            }
+            level++;
+        }
+
+        int ans = 0;
+        for(int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                if (grid[row][col] == -1)
+                    return -1;
+                ans = Math.max(ans, grid[row][col]);
+            }
+        }
+        return ans;
+    }
+}
