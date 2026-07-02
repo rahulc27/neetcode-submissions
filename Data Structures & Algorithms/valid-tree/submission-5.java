79f631d53e267de6df3e8ee08fb423ae1d455983
Rahul Chavan <32196872+rahulc27@users.noreply.github.com>
2026-07-02 17:29:28 +0530

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.java
@@ -0,0 +1,31 @@
+class Solution {
+    HashMap<Integer, List<Integer>> adjMap = new HashMap();
+    Set<Integer> visited = new HashSet();
+
+    public boolean validTree(int n, int[][] edges) {
+        for(int[] edge : edges) {
+            if(!adjMap.containsKey(edge[0]))
+                adjMap.put(edge[0], new ArrayList());
+            if(!adjMap.containsKey(edge[1]))
+                adjMap.put(edge[1], new ArrayList());
+            adjMap.get(edge[0]).add(edge[1]);
+            adjMap.get(edge[1]).add(edge[0]);
+        }
+            
+        return dfs(0, -1) && visited.size() == n;
+    }
+
+    public boolean dfs(int currNode, int prevNode) {
+        if (visited.contains(currNode))
+            return false;
+        
+        visited.add(currNode);
+        for(int nextNode : adjMap.getOrDefault(currNode, Collections.emptyList()))
+            if (nextNode != prevNode) {
+                if (!dfs(nextNode, currNode))
+                    return false;
+            }
+                
+        return true;
+    }
+}
